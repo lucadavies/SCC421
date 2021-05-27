36c00bab893030fc710df7ea7d6dbd857ef257ea
Luca Davies <luca-davies@outlook.com>
2021-05-27 01:01:44 +0100

    Updates to working materials

--- Run SonarQube on samples.ps1
+++ Run SonarQube on samples.ps1
@@ -1,7 +1,9 @@
+Set-PSDebug -Trace 1
 cd "Code Samples"
 sonar-scanner
 Move-Item sonar-project.properties ..
-dotnet "C:\Users\Luca\Documents\SonarScanner for .NET\SonarScanner.MSBuild.dll" begin /k:codeSamplesCS /d:sonar.login="a69dbe670d69bea87a21b87ebdcf9fd2f5651653"
+dotnet "C:\Users\Luca\Documents\SonarScanner for .NET\SonarScanner.MSBuild.dll" begin /k:codeSamplesCS /d:sonar.login="6f0379bd8475d24b9994ecf5c14be9b4db41ac26"
 dotnet build "C:\Users\Luca\Documents\University\General\UG4\SCC421\Code Samples\C#\Example\Example.sln"
-dotnet "C:\Users\Luca\Documents\SonarScanner for .NET\SonarScanner.MSBuild.dll" end /d:sonar.login="a69dbe670d69bea87a21b87ebdcf9fd2f5651653"
+dotnet "C:\Users\Luca\Documents\SonarScanner for .NET\SonarScanner.MSBuild.dll" end /d:sonar.login="6f0379bd8475d24b9994ecf5c14be9b4db41ac26"
 Move-Item ..\sonar-project.properties .
+cd ".."
