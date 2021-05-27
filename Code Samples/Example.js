e7c0fc8a790388fb288946a1bf6e4297856dfb62
Luca Davies <luca-davies@outlook.com>
2021-05-27 01:02:01 +0100

    Improve sample code

--- Code Samples/Example.js
+++ Code Samples/Example.js
@@ -1,92 +1,192 @@
-// TERNARY
-    //1
-    function getFee(isMember)
+class Example
+{
+    // TERNARY
+    static ternaryAdv(temp)
     {
-        return (isMember ? '$2.00' : '$10.00');
+        return temp < 20.0 ? "Cold." : "Perfect!";
     }
 
-    function getFee(isMember)
+    static ternaryPlain(temp)
     {
-        if (isMember)
+        if (temp < 20.0)
         {
-            return '$2.00';
+            return "Cold.";
         }
         else
         {
-            return '$10.00';
+            return "Perfect!";
         }
     }
 
-// ANONYMOUS FUNCTIONS
-    var p100 = a => a + 100;
-    //-
-    function plus100(a)
+    // LAMBDA/ARROW FUNCTIONS
+    static lambdaAdv(name)
     {
-        return a + 100;
-    }
-// NULL-COALESCE
-    const maybeNullValue = null;
-    //setup
-    var valA = maybeNullValue ?? "default for A";
-    //-
-    if (maybeNullValue != null)
-    {
-        valA = maybeNullValue;
-    }
-    else
-    {
-        valA = "default for A";
-    }
-
-// NULL-CONDITIONAL
-    const adventurer =
-    {
-        name: 'Alice',
-        cat:
+        let foo = s =>
         {
-            name: 'Dinah'
+            let greeting = "Hello " + s + "!";
+            console.log(greeting);
         }
-    };
-    var dogName;
-    //setup
-    dogName = adventurer.dog?.name;
-    //-
-    if (adventurer.dog != undefined)
-    {
-        dogName = adventurer.dog.name;
+        
+        foo(name);
     }
     
-// UNARY INC/DECR.
-    var x;
-    var y;
-    var z
-    //setup
-    x = 3;
-    y = x++;
-    z = ++x;
-    //-
-    x = 3;
-    y = x;
-    x = x + 1;
-    z = x + 1;
-    x = x + 1;
+    static lambdaPlain(name)
+    {
+        let greeting = "Hello " + name + "!";
+        console.log(greeting);
+    }
 
-// COMPOUND ASSIGNMENT
-    var a = 2;
-    //setup
-    a += 3;
-    //-
-    a = a + 3;
-// FOREACH
-    let cars = ["BMW", "Volvo", "Mini"];
-    let text = "";
-    //setup
-    for (let x of cars)
+    // NULL-COALESCE
+    static nullCoalesceAdv()
     {
-        text += x + "<br>";
+        let a = null;
+        let b = null;
+        b = a ?? -1;
+
+        console.log(b);
     }
-    //-
-    for (var i = 0; i < cars.length; i++)
+
+    static nullCoalescePlain()
     {
-        text += cars[i] + "<br>"
+        let a = null;
+        let b = null;
+        if (a != null)
+        {
+            b = a;
+        }
+        else
+        {
+            b = -1;
+        }
+
+        console.log(b);
     }
+
+    // NULL-CONDITIONAL
+    static nullConditionalAdv()
+    {
+        let p = new Person();
+        //setup
+        let name = "p is null, no name";
+        
+        name = p?.firstName;
+
+        console.log(name);
+    }
+
+    static nullConditionalPlain()
+    {
+        let p = new Person();
+        //setup
+        let name = "p is null, no name";
+
+        if (p != null)
+        {
+            name = p.firstName;
+        }
+
+        console.log(name);
+    }
+
+    // UNARY INC/DECR.
+    static incrDecrAdv()
+    {
+        let x = 0;
+        let y = 0;
+        let z = 0;
+        //setup
+        x = 3;
+        y = x++;
+        z = ++x;
+
+        console.log(x);
+        console.log(y);
+        console.log(z);
+    }
+
+    static incrDecrPlain()
+    {
+        let x = 0;
+        let y = 0;
+        let z = 0;
+        //setup
+        x = 3;
+
+        y = x;
+        x = x + 1;
+
+        z = x + 1;
+        x = x + 1;
+
+        console.log(x);
+        console.log(y);
+        console.log(z);
+    }
+
+    // COMPOUND ASSIGNMENT
+    static compoundAssignAdv()
+    {
+        let x = 2;
+        //setup
+        x += 3;
+        console.log(x);
+    }
+
+    static compoundAssignPlain()
+    {
+        let x = 2;
+        //setup
+        x = x + 3;
+        console.log(x);
+    }
+
+    // FOREACH
+    static foreachAdv()
+    {
+        let nums = [10, 99, 4, 65, 38, 42];
+        let total = 0;
+        //setup
+        for (let i of nums)
+        {
+            total += i;
+        }
+
+        console.log(total);
+    }
+
+    static foreachPlain()
+    {
+        let nums = [10, 99, 4, 65, 38, 42];
+        let total = 0;
+        //setup
+        for (let i = 0; i < nums.length; i++)
+        {
+            total += nums[i];
+        }
+
+        console.log(total);
+    }
+}
+
+class Person
+{
+    constructor(fn)
+    {
+        this.firstName = fn;
+    }
+}
+
+Example.ternaryAdv(10);
+Example.ternaryPlain(10);
+Example.lambdaAdv("Alice");
+Example.lambdaPlain("Bob");
+Example.nullCoalesceAdv();
+Example.nullCoalescePlain();
+Example.nullConditionalAdv();
+Example.nullConditionalPlain();
+Example.incrDecrAdv();
+Example.incrDecrPlain();
+Example.compoundAssignAdv();
+Example.compoundAssignPlain();
+Example.foreachAdv();
+Example.foreachPlain();
